782588b88ce3d400301ae088e7224e5b80671177
coryholley <courtneynholley@gmail.com>
2020-09-28 15:51:10 -0500

    first Java exercise complete

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,51 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.print("Hello, World!");
+//        System.out.println("Hello, World!");
+//
+//        int myFavoriteNumber = 11;
+//        System.out.println(myFavoriteNumber);
+//
+//        String myString = "Today is Monday";
+//        System.out.println(myString);
+//
+//        float myNumber = 3.14F;
+//        System.out.println(myNumber);
+//
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        System.out.println(three);
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x += 5;
+//        System.out.println(x);
+
+//        int x = 3;
+//        int y = 4;
+//        y *= x;
+//        System.out.println(y);
+
+        int x = 10;
+        int y = 2;
+        x /= y;
+        y -= x;
+        System.out.println(x);
+        System.out.println(y);
+
+        byte testNum = 126;
+        testNum++;
+        testNum++;
+        System.out.println(testNum);
+
+
     }
 }
