f1ee2f788361792a642c65f71d7f1014f68e34fb
coryholley <courtneynholley@gmail.com>
2020-09-29 15:15:00 -0500

    fixed While Loop exercise a to print on one line

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -1,8 +1,9 @@
+
 public class ControlFlowExercises {
     public static void main(String[] args) {
         int i = 5;
         while (i <= 15) {
-            System.out.println(i);
+            System.out.print(i + " ");
             i++;
         }
     }
