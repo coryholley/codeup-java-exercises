4354d8325d733738f75480de8fc7ff0a55881a63
coryholley <courtneynholley@gmail.com>
2020-09-29 12:05:45 -0500

    fixed the three word problem

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class ConsoleExercises {
     public static void main(String[] args) {
-//        double pi = 3.14159;
-//        System.out.format("The value of pi is approximately %.2f%n", pi);
+        double pi = 3.14159;
+        System.out.format("The value of pi is approximately %.2f%n", pi);
 
         Scanner scanner = new Scanner(System.in);
         scanner.useDelimiter("\n");
@@ -12,12 +12,12 @@ public class ConsoleExercises {
 //
 //        System.out.println("You entered: --> \"" + userInput + "\" <--");
 
-//        System.out.print("Enter three words: ");
+//        System.out.println("Enter three words: ");
 //        String wordOne = scanner.next();
 //        String wordTwo = scanner.next();
 //        String wordThree = scanner.next();
 //
-//        System.out.println("You entered: --> \"" + wordOne + "\", \"" + wordTwo + "\", \"" + wordThree + "\" <--");
+//        System.out.printf("The first word entered: %s.%n The second word entered: %s.%n The third word entered: %s.%n", wordOne, wordTwo, wordThree);
 
 //        System.out.println("Enter a sentence: ");
 //        String userSentence = scanner.nextLine();
