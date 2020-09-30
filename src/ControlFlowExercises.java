625836e3561448fa6ded6340c9be906970a82e89
coryholley <courtneynholley@gmail.com>
2020-09-29 20:54:54 -0500

    Feat: finished part two of c. do-while loop to for loop.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,8 +2,8 @@
 
 public class ControlFlowExercises {
     public static void main(String[] args) {
-        for(int i = 5; i <= 15; i++) {
-            System.out.print(i + " ");
+        for(long i = 2; i <= 1000000; i *= i) {
+            System.out.println(i);
         }
     }
 }
