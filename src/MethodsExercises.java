fe8d8d0519bdcae527bbdfdd271f13b72f5cf422
coryholley <courtneynholley@gmail.com>
2020-10-01 13:00:42 -0500

    Feat: Problem 2. Might need reworking.

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,24 +2,26 @@ import java.util.Scanner;
 
 public class MethodsExercises {
 
-    public static long getPower(int base, int exponent) {
-        if (exponent == 0) {
-            return 1;
-        } else if (exponent == 1) {
-            return base;
-        } else if (exponent == 2) {
-            return base * base;
-        }
-        return base * getPower(base, exponent - 1);
-    }
+    Scanner scanner = new Scanner(System.in);
 
-    public static long addition(long base, long add) {
-        return base + add;
-    }
-
-    public static long subtraction(long base, long sub) {
-        return base - sub;
-    }
+//    public static long getPower(int base, int exponent) {
+//        if (exponent == 0) {
+//            return 1;
+//        } else if (exponent == 1) {
+//            return base;
+//        } else if (exponent == 2) {
+//            return base * base;
+//        }
+//        return base * getPower(base, exponent - 1);
+//    }
+//
+//    public static long addition(long base, long add) {
+//        return base + add;
+//    }
+//
+//    public static long subtraction(long base, long sub) {
+//        return base - sub;
+//    }
 
 //    public static long multiplication(long base, long mult) {
 //        return base * mult;
@@ -33,37 +35,54 @@ public class MethodsExercises {
 //        return result;
 //    }
 
-    public static long multiplication(long base, long mult) {
-            if (mult == 0) {
-                return 0;
-            } else if (mult == 1) {
-                return base;
-            } else if (mult == 2) {
-                return base + base;
-            }
-            return base + multiplication(base, mult - 1);
-    }
+//    public static long multiplication(long base, long mult) {
+//            if (mult == 0) {
+//                return 0;
+//            } else if (mult == 1) {
+//                return base;
+//            } else if (mult == 2) {
+//                return base + base;
+//            }
+//            return base + multiplication(base, mult - 1);
+//    }
+//
+//    public static long division(long base, long divis) {
+//        return base / divis;
+//    }
+//
+//    public static long modulus(long base, long mod) {
+//        return base % mod;
+//    }
 
-    public static long division(long base, long divis) {
-        return base / divis;
-    }
+//    public static int getInteger(int min, int max) {
+//        Scanner scanner = new Scanner(System.in);
+//        System.out.print("Enter a number between 1 and 10: \n");
+//        int userInput = scanner.nextInt();
+//
+//        if (userInput < min || userInput > max) {
+//            return userInput = getInteger(1,10);
+//        } else {
+//            return userInput;
+//        }
+//    };
 
-    public static long modulus(long base, long mod) {
-        return base % mod;
-    }
 
 
     public static void main(String [] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println(getPower(2,4));
+//        System.out.println(getPower(2,4));
+//
+//        System.out.println(addition(5,5));
+//        System.out.println(subtraction(20, 10));
+//        System.out.println(multiplication(20,6));
+//        System.out.println(division(20,2));
+//        System.out.println(modulus(3,2));
 
-        System.out.println(addition(5,5));
-        System.out.println(subtraction(20, 10));
-        System.out.println(multiplication(20,6));
-        System.out.println(division(20,2));
-        System.out.println(modulus(3,2));
+//        System.out.print(getInteger(1, 10));
 
+//        int userInput = getInteger(1, 10);
+//        System.out.println(userInput);
 
 
     }
