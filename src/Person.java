e2d6b0fbac7061141427d4b03cc68e223ac60407
coryholley <courtneynholley@gmail.com>
2020-10-05 14:12:28 -0500

    finished exercise 1 of oop

--- src/Person.java
+++ src/Person.java
@@ -24,11 +24,28 @@ private String name;
     }
 
     public static void main(String[] args) {
-        Person person1 = new Person("Cory");
+//        Person person1 = new Person("Cory");
+//
+//        System.out.println(person1.getName());
+//        person1.setName("Tyler");
+//        System.out.println(person1.getName());
+//        person1.sayHello();
 
+//        Person person1 = new Person("John");
+//        Person person2 = new Person("John");
+//        System.out.println(person1.getName().equals(person2.getName()));
+//        System.out.println(person1 == person2);
+
+//        Person person1 = new Person("John");
+//        Person person2 = person1;
+//        System.out.println(person1 == person2);
+
+        Person person1 = new Person("John");
+        Person person2 = person1;
         System.out.println(person1.getName());
-        person1.setName("Tyler");
+        System.out.println(person2.getName());
+        person2.setName("Jane");
         System.out.println(person1.getName());
-        person1.sayHello();
+        System.out.println(person2.getName());
     }
 }
