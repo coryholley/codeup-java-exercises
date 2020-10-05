5f2d6b4e2fa76a35923acdb7d191069db198c6a5
coryholley <courtneynholley@gmail.com>
2020-10-05 14:04:57 -0500

    First practice exercise for OOP

---
+++ src/OOP/DishTest.java
@@ -0,0 +1,19 @@
+package OOP;
+
+public class DishTest {
+
+    public static void main(String[] args) {
+ //        Create another class called DishTest
+//         Add a main method and inside the method...
+//        1) instantiate a Dish object and assign to a variable called dish1
+//        2) assign creative values for each of the properties
+//        3) test the printSummary() method by invoking it and checking if all instance values are correctly printed
+
+        Dish dish1 = new Dish();
+        dish1.costInCents = 2000;
+        dish1.nameOfDish = "Truffle Pasta";
+        dish1.wouldRecommend = true;
+
+        dish1.printSummary();
+    }
+}
