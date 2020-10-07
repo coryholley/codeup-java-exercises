7b29fec8f7fd6170940f1076f0cbd62577b7ab2d
coryholley <courtneynholley@gmail.com>
2020-10-07 12:28:57 -0500

    Feat: Completed Exercise 2

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+
+import util.Input;
+
+public class ServerNameGenerator {
+
+    private static String[] adjectives = {"beautiful", "adorable", "short", "tall", "round", "perfect", "tired", "lazy", "old", "young"};
+
+    private static String[] nouns = {"brother", "sister", "plant", "building", "playground", "kitchen", "store", "street", "park", "home"};
+
+    public static String randomReturn(String[] array) {
+        return array[(int) (Math.random() * array.length)];
+    }
+
+    public static void main(String[] args) {
+        boolean toContinue;
+
+        do {
+            System.out.println(randomReturn(ServerNameGenerator.adjectives) + "-" + randomReturn(ServerNameGenerator.nouns));
+            toContinue = new Input().yesNo("Would you like to continue? [y/n]");
+        } while (toContinue);
+    }
+}
