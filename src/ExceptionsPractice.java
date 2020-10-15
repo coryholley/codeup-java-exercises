c1ca9f62443c8fce371a8ca26e9928f6964fa427
coryholley <courtneynholley@gmail.com>
2020-10-15 10:01:14 -0500

    Feat: Made some code throw out exceptions

---
+++ src/ExceptionsPractice.java
@@ -0,0 +1,18 @@
+public class ExceptionsPractice {
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4};
+
+
+//        System.out.println(numbers[4]);
+
+//        if (numbers.length < 2 == false) {
+//            throw new IllegalArgumentException("Error");
+//        }
+
+//        int sum = 2/0;
+
+
+        }
+    }
+}
